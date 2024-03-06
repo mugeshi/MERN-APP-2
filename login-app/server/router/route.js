50f96176f13c446b9adcbac5aeb75367466e66cd
mugeshi <nicolemugeshi@gmail.com>
2024-03-06 22:06:59 +0300

    updated routes

--- login-app/server/router/route.js
+++ login-app/server/router/route.js
@@ -5,11 +5,21 @@ const router = Router();
 
 //POST Method
 router.route('/register').post((req, res) =>res.json('register route'));
+router.route('/registerMail').post();
+router.route('/authenticate').post();
+router.route('/login').post();
 
 
 //GET Method
+router.route('/user/:username').get ()
+router.route('/generateOTP').get()
+router.route('/verifyOTP').get()
+router.route('/createResetSession').get()
 
 
+//PUT Method
+router.route('/updateuser').put();
+router.route('/resetPassword').put();
 
 
 export default router;
