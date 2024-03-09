bc1d598de20118f844e337c3806c8fccda98a70f
mugeshi <nicolemugeshi@gmail.com>
2024-03-09 22:43:08 +0300

    updated the UseSchema

--- login-app/server/model/User.model.js
+++ login-app/server/model/User.model.js
@@ -8,5 +8,21 @@ export const UserSchema = new mongoose.Schema({
     required: [true, "Please provide a unique username"],
     unique: [true, "Username already exists"],
   },
+  password : {
+    type: String,
+    required: [true, "Please provide a password"],
+    unique: true,
+  },
+  email:{
+    type: String,
+    required: [true, "please provide a unique email"]
+  },
+  firstName: {type: String},
+  lastName: {type: String},
+  mobile: {type: Number},
+  address: {type: String},
+  profile: {type: String}
   
 });
+
+export default mongoose.model.Users || mongoose('User', UserSchema);
